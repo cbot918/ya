a4909d9ae05da029a52976105941f031609faba5
cbot918 <cbot918@gmail.com>
2023-02-19 23:27:14 +0800

    fix_main.go_and_release_v0.0.3

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
-import "github.com/cbot918/liby/cmdy"
+import (
+	"github.com/cbot918/ya/src/ya"
+	// "github.com/cbot918/liby/cmdy"
+)
+
+
 
 func main(){
-	c := cmdy.New()
-	script1 := []string{"git clone -b go https://github.com/cbot918/template"}
-	script2 := []string{"rm -rf template"}
-	c.Run(script1)
-	c.Run(script2)
-	
+	// c := cmdy.New()
+	// script1 := []string{"git clone -b go https://github.com/cbot918/template"}
+	// script2 := []string{"rm -rf template"}
+	// c.Run(script1)
+	// c.Run(script2)
+
+	y := ya.New()
+	y.Run()
+
+	// branch 
+	// name
+	// rm -rf .git && git init
+	// git add .
+	// git commit -m "init project"
+	// gh repo create $name --public --add-readme --push --source .
+
 }
