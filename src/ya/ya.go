69bd6f95eb1ab02ded66014a7602605ef5fb6556
cbot918 <cbot918@gmail.com>
2023-02-20 02:47:27 +0800

    update

--- src/ya/ya.go
+++ src/ya/ya.go
@@ -40,7 +40,7 @@ func (y *Ya) Run(){
 					})
 				}
 			}
-			case "pre": {
+			case "install-gh": {
 				if len(args) == 2 {
 
 						fmt.Println("pre-install start...")
@@ -69,67 +69,8 @@ func (y *Ya) Run(){
 								fmt.Sprintf("rm -rf %s && rm %s.tar.gz",y.GhName,y.GhName),
 							})
 						}
-				} 
-				// if args[2] == "git" {
-				// 	c.Run([]string{""})
-					
-				// }
-				// if args[2]== "gh" {
-					
-				// }
-		
-				// if args[2] == "docker"{
-				// 	c.Run([]string{
-				// 		"apt update && apt install curl -y",
-				// 		fmt.Sprintf("curl -OL %s && tar -xvf %s.tar.gz",y.GhUrl, y.GhName),
-				// 		fmt.Sprintf("mkdir -p /usr/local/bin && cp %s/bin/gh /usr/local/bin ",y.GhName),
-				// 		fmt.Sprintf("rm -r %s && rm %s.tar.gz",y.GhName,y.GhName ),
-				// 		"git config --global user.name 'cbot918'",
-				// 		"git config --global user.email 'cbot918@gmail.com'",
-				// 	})
-				// }
-				// if args[2] == "vm" {
-				// 	c.Run([]string{
-				// 		"sudo apt update && sudo apt install curl",
-				// 		fmt.Sprintf("curl -OL %s && tar -xvf %s.tar.gz",y.GhUrl, y.GhName),
-				// 		fmt.Sprintf("mkdir -p /usr/local/bin && sudo cp %s/bin/gh /usr/local/bin ",y.GhName),
-				// 		fmt.Sprintf("rm -rf %s && rm %s.tar.gz",y.GhName,y.GhName),
-				// 		"git config --global user.name 'cbot918'",
-				// 		"git config --global user.email 'cbot918@gmail.com'",
-				// 	})
-				// }
-		
+				} 		
 			}
-		
-			// case "pre-install":{
-			// 	if args[2] == "."{
-			// 		u.Logg("in ......")
-			// 	}else {
-			// 		projectName = args[2]
-			// 		c.Run([]string{"git clone -b go https://github.com/cbot918/template"})
-			// 		c.Run([]string{fmt.Sprintf("mv template %s", projectName)})
-			// 	}
-			// }
-			case "gitc":{
-				u.Logg("in gitc")
-				if args[2] == "."{
-					u.Logg("in gitc .")
-					c.Run([]string{
-						"rm -rf .git && git init",
-						"git checkout -b main",
-						// fmt.Sprintf("echo \"# %s\" > README.md", projectName ),
-						"git add .",
-						"git commit -m 'init project'",
-						"echo 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 | gh auth login --with-token",
-						"gh repo create --public --push --source .",
-					})
-				}else{
-					fmt.Println("後面+個點")
-					return
-				}}
 			}
-
 	}
-
-
 }
